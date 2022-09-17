b0ddf38f2ac790c04e9252655d6031fc4ff2df08
hey fixit <heyjuststart@gmail.com>
2022-09-17 00:34:33 -0400

    add types for createPosts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,3 +1,4 @@
+import { Post } from "@prisma/client";
 import { prisma } from "../db.server";
 
 export async function getPostListings() {
@@ -21,7 +22,9 @@ export async function getPost(slug: string) {
   });
 }
 
-export async function createPost(post) {
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
   return prisma.post.create({
     data: post,
   });
--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,6 +1,15 @@
 import { Form, useActionData } from "@remix-run/react";
 import { ActionFunction, json, redirect } from "@remix-run/server-runtime";
+import invariant from "tiny-invariant";
 import { createPost } from "~/models/post.server";
+
+type ActionData =
+  | {
+    title: string | null;
+    slug: string | null;
+    markdown: string | null;
+  }
+  | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
   const data = await request.formData();
@@ -8,7 +17,7 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = data.get("slug");
   const markdown = data.get("markdown");
 
-  const errors = {
+  const errors: ActionData = {
     title: !title ? "Title is required" : null,
     slug: !slug ? "Slug is required" : null,
     markdown: !markdown ? "Markdown is required" : null,
@@ -17,8 +26,12 @@ export const action: ActionFunction = async ({ request }) => {
   // if any of the values were truthy we know there was at least one error
   const hasErrors = Object.values(errors).some((error) => error);
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
+
+  invariant(typeof title === "string", "title should be a string");
+  invariant(typeof slug === "string", "slug should be a string");
+  invariant(typeof markdown === "string", "markdown should be a string");
 
   await createPost({ title, slug, markdown });
   return redirect("/posts/admin");
@@ -27,7 +40,7 @@ export const action: ActionFunction = async ({ request }) => {
 const inputClassName = `w-full rounded border border-gray-300 px-3 py-2 mb-3 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:border-transparent`;
 
 export default function NewPostRoute() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
   return (
     <Form method="post">
       <p>
